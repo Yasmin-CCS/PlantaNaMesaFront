be80b7e71ab357fcaa9217ab7f5f01350e7cd269
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-05-31 15:54:11 -0300

    Correção DetalheProduto

--- src/store/tokens/Action.ts
+++ src/store/tokens/Action.ts
@@ -1,26 +1,26 @@
 import Produto from "../../models/Produto";
 
-export type Action = {type: "ADD_TOKEN"|"ADD_ID"|"REMOVE_ITEM"| "ADD_TO_CART"; payload:any}
-
+export type Action = {
+  type: "ADD_TOKEN" | "ADD_ID" | "REMOVE_ITEM" | "ADD_TO_CART";
+  payload: any
+};
 
 export const addToken = (token: string): Action => ({
   type: "ADD_TOKEN",
-  payload: token
-})
+  payload: token,
+});
 
-//pegar o id do usuario na hora do login
 export const addId = (id: string): Action => ({
   type: "ADD_ID",
-  payload: id
-})
+  payload: id,
+});
 
 export const addToCart = (id: Produto): Action => ({
   type: "ADD_TO_CART",
-  payload: id
+  payload: id,
+});
 
-})
-
-export const removeItem = (id: Produto []): Action => ({
+export const removeItem = (id: Produto[]): Action => ({
   type: "REMOVE_ITEM",
-  payload: id
-})
+  payload: id,
+});
