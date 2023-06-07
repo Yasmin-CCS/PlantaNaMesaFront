76a81b47d553b0fda19f1ea29cf15785da507151
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-06-07 19:43:46 -0300

    Tentativa de criação de botão retirar prod do car

--- src/components/carrinho/Carrinho.tsx
+++ src/components/carrinho/Carrinho.tsx
@@ -1,11 +1,14 @@
 import { toast } from "react-toastify";
-import { removeItem } from "../../store/tokens/Action";
+import { addToCart, removeItem, removeToCart } from "../../store/tokens/Action";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "@material-ui/core";
 import { Link, useNavigate } from "react-router-dom";
 import { TokenState } from "../../store/tokens/TokensReducer";
 import { useEffect } from "react";
 import { Box, Grid, Typography } from "@mui/material";
+import './Carrinho.css';
+import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+
 
 function Carrinho() {
   const navigate = useNavigate();
@@ -51,25 +54,40 @@ function Carrinho() {
     navigate("/home");
   }
 
+  function removeFromCart(id: number) {
+    dispatch(removeToCart(carrinho.filter((item) => { return item.id !== id })));
+    toast.success("Produto removido com sucesso!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "colored",
+      progress: undefined,
+    });
+    navigate("/carrinho")
+  }
+
+
+
   return (
     <>
- 
       <Typography align="center" variant="h4">
         Produtos Adicionados
       </Typography>
-      
 
       {carrinho.map((item) => (
         <>
-        
           <Grid container my={2} px={4} alignItems="center">
             <Box display="flex" flexWrap={"wrap"} width={"100%"}>
               <Grid item xs={2} border={2} borderRadius={1} borderColor={"black"} p={1}>
                 <Typography>{item.nome}</Typography>
-
-                <img src={item.foto} alt={item.nome} width={200} height={200}/>
-              
-                <Typography>Valor: {item.valor}</Typography>
+                <img className="img_listCart" src={item.foto} alt={item.nome} />
+                <Typography>Valor: R$ {item.valor}</Typography>
+                <Button type="submit" color="primary" onClick={() => removeFromCart(item.id)}>
+                  <DisabledByDefaultIcon />
+                </Button>
               </Grid>
             </Box>
           </Grid>
@@ -78,32 +96,32 @@ function Carrinho() {
 
       <>
 
-      <Typography align="center">
-        {" "}
-        {carrinho.map((price) => {
-          {
-            valorTotal = valorTotal + price.valor;
-          }
-        })}
+        <Typography align="center">
+          {" "}
+          {carrinho.map((price) => {
+            {
+              valorTotal = valorTotal + price.valor;
+            }
+          })}
 
-        <h3>Valor Total: R${valorTotal}</h3>
+          <h3>Valor Total: R${valorTotal}</h3>
 
         </Typography>
         <Box marginTop={2} textAlign="center">
-              <Link to="/produtos">
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  className="btnCancelar"
-                >
-                  Voltar
-                </Button>
-              </Link>
+          <Link to="/produtos">
+            <Button
+              variant="contained"
+              color="secondary"
+              className="btnCancelar"
+            >
+              Voltar
+            </Button>
+          </Link>
 
-              <Button type="submit" variant="contained" color="primary" onClick={buy}>
-              Comprar
-              </Button>
-            </Box>
+          <Button type="submit" variant="contained" color="primary" onClick={buy}>
+            Comprar
+          </Button>
+        </Box>
       </>
     </>
   );
--- src/store/tokens/TokensReducer.ts
+++ src/store/tokens/TokensReducer.ts
@@ -1,38 +1,55 @@
-import Produto from '../../models/Produto'
-import { Action } from './Action'
+import Produto from "../../models/Produto";
+import { Action } from "./Action";
 
 export interface TokenState {
-  token: string,
-  id: string
+  token: string;
+  id: string;
 
-  produtos: Array<Produto>
+  produtos: Array<Produto>;
 }
 
 const initialState = {
-  token: '',
-  id: '',
-  produtos: []
+  token: "",
+  id: "",
+  produtos: [],
+};
+const updatedState={
+  token: initialState.token,
+  id: "",
+  produtos: [],
 }
 
-export const tokenReducer = (state: TokenState = initialState, action: Action) => {
+export const tokenReducer = (
+  state: TokenState = initialState,
+  action: Action
+) => {
   switch (action.type) {
     case "ADD_TOKEN": {
-      return {...state, token: action.payload}
+      return { ...state, token: action.payload };
     }
     case "ADD_ID": {
-      return {...state, id: action.payload }
+      return { ...state, id: action.payload };
     }
     case "ADD_TO_CART": {
       return {
-        ...state, produtos: [...state.produtos, action.payload]
-      }
+        ...state,
+        produtos: [...state.produtos, action.payload],
+      };
     }
     case "REMOVE_ITEM": {
       return {
-        ...state, produtos: []
-      }
+        ...state,
+        produtos: [],
+      };
+    }
+    case "REMOVE_TO_CART": {
+      return {
+        ...state,
+        produtos: [...state.produtos, action.payload],
+      };
     }
 
-    default: return state
+    default:
+      return state;
   }
-}
+};
